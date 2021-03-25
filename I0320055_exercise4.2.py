d6c4699d83bf55011021912088a451a7252254e4
L Larasfadani <larasfadani@gmail.com>
2021-03-25 21:35:37 +0700

    Soal 1: exercise 4.1 sampai 4.12

---
+++ I0320055_exercise4.2.py
@@ -0,0 +1,16 @@
+x = 15
+x = 10
+y = 12
+
+#output: x > y is false
+print('x > y is', x>y)
+#output: x < y is false
+print('x < y is', x<y)
+#output: x == y is false
+print('x == y is', x==y)
+#output: x != y is false
+print('x != y is', x!=y)
+#output: x >= y is false
+print('x >= y is', x>=y)
+#output: x <= y is false
+print('x <= y is', x<=y)
